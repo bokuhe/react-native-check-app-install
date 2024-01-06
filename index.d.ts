a32c022cc2e8d885f8f228f13326fea07d6ff17d
H. Park <bokuhe@gmail.com>
2024-01-06 16:27:01 +0900

    feat: add ts types

---
+++ index.d.ts
@@ -0,0 +1,18 @@
+declare module 'react-native-check-app-install' {
+  export class AppInstalledChecker {
+    static getAppList(): string[];
+    static checkPackageName(packagename: string): Promise<boolean>;
+    static checkURLScheme(proto: string, query?: string): Promise<boolean>;
+    static isAppInstalled(key: string): Promise<boolean>;
+    static isAppInstalledAndroid(key: string): Promise<boolean>;
+    static isAppInstalledIOS(key: string): Promise<boolean>;
+  }
+
+  export const CheckPackageInstallation: {
+    isPackageInstalled(
+      packagename: string,
+      callback: (isInstalled: boolean) => void
+    ): void;
+  };
+}
+
